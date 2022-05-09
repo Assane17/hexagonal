9e729020eb8ce23d94d9a65211f0f7a282c2aa57
Assane17 <a.dione@iatf.it>
2022-05-09 19:00:31 +0000

    commit three

--- create_post.php
+++ create_post.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Controller\CreatePostController;
+use Domain\Forum\Actions\CreatePost;
 use Symfony\Component\HttpFoundation\Request;
+use Test\Adapters\InMemoryPostRepository;
 
 require __DIR__.'/vendor/autoload.php';
 $request = Request::createFromGlobals();
-$controller =  new CreatePostController();
+$repository = new InMemoryPostRepository();
+$action = new CreatePost($repository);
+$controller =  new CreatePostController($action);
 $response = $controller->handleRequest($request);
 $response->send();
--- infra/Controller/CreatePostController.php
+++ infra/Controller/CreatePostController.php
@@ -1,20 +1,38 @@
 <?php
 
 namespace App\Controller;
+use DateTime;
+use Domain\Forum\Actions\CreatePost;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class CreatePostController
 {
+    protected CreatePost $createPost;
+
+    /**
+     * @param CreatePost $createPost
+     */
+    public function __construct(CreatePost $createPost)
+    {
+        $this->createPost = $createPost;
+    }
+
     public function handleRequest(Request $request)
     {
         if($request->isMethod('GET')){
             ob_start();
-            include __DIR__.'../templates/form.html.php';
+            include __DIR__.'/../templates/form.html.php';
             return new Response(ob_get_clean());
         }
 
+      $post =  $this->createPost->execute([
+          'titre' => $request->request->get('titre'),
+          'description' => $request->request->get('description'),
+          'createdAt' =>new DateTime('now'),
+          'published' => (int)$request->request->get('published') == 1,
+      ]);
 
-
+        return new Response("<h1>{$post->titre}</h1>");
     }
 }
